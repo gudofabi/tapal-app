7d582d1fba77c777a06cbd5480a7fa31ca7b456c
gudofabi <godofredocpanel@gmail.com>
2025-02-21 21:40:27 +0800

    fix: add loader for resources

--- stores/loading.ts
+++ stores/loading.ts
@@ -4,9 +4,23 @@ export const useLoadingStore = defineStore("loadingStore", () => {
   const startLoading = () => {
     isLoading.value = true;
   };
-  const stoptLoading = () => {
+
+  const stopLoading = () => {
     isLoading.value = false;
   };
 
-  return { isLoading, startLoading, stoptLoading };
+  const preloadImages = (): Promise<void> => {
+    const images = Array.from(document.querySelectorAll("img")); // Get all images in the DOM
+    const promises = images.map((img) => {
+      if ((img as HTMLImageElement).complete) return Promise.resolve(); // If already loaded, resolve immediately
+      return new Promise<void>((resolve) => {
+        (img as HTMLImageElement).onload = () => resolve();
+        (img as HTMLImageElement).onerror = () => resolve(); // Resolve even if there's an error
+      });
+    });
+
+    return Promise.all(promises).then(() => {});
+  };
+
+  return { isLoading, startLoading, stopLoading, preloadImages };
 });
